fb1ff71c12f86fca980597f717ffa6fddde99540
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-07-14 19:00:20 +0000

    Adding compatibility for video transcripts

---
+++ edx_s3_compatibility/__init__.py
@@ -0,0 +1,15 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.core.files.base import ContentFile
+
+class S3Boto3StorageWrapper(S3Boto3Storage):
+    def __force_byte_content(self, content):
+        block = content.read(1024)
+        content.seek(0)
+
+        if not isinstance(block, bytes):
+            return ContentFile(content.read().encode())
+        return content
+
+    def save(self, name, content, max_length=None):
+        content = self.__force_byte_content(content)
+        return super().save(name, content, max_length)
